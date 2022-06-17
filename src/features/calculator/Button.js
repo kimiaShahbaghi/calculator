00395ed9e903c2600ac03ea3d279b17e2d46a695
kimiaShahbaghi <kshahbaghi@gmail.com>
2022-06-17 21:20:08 +0430

    add functionality

--- src/features/calculator/Button.js
+++ src/features/calculator/Button.js
@@ -1,32 +1,60 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { addInput, calc, singleOperator, acDelete } from "./calculatorSlice";
 import "./Button.scss";
 
-function Button(props) {
-  const classPicker = (value) => {
-    switch (value) {
-      case "(":
-      case ")":
-      case "%":
-      case "AC":
-      case "/":
-      case "*":
-      case "-":
-      case "+":
-        return "operator";
+const classPicker = (value) => {
+  switch (value) {
+    case "(":
+    case ")":
+    case "%":
+    case "AC":
+    case "/":
+    case "*":
+    case "-":
+    case "+":
+      return "operator";
 
+    case "=":
+      return "equal";
+
+    default:
+      return "number";
+  }
+};
+
+function Button(props) {
+  const dispatch = useDispatch();
+
+  const clickHandler = (input) => {
+    switch (input) {
       case "=":
-        return "equal";
+        console.log("equal");
+        dispatch(calc());
+        break;
+
+      case "*":
+      case "/":
+      case "+":
+      case "-":
+      case ".":
+        dispatch(singleOperator(input));
+        break;
+
+      case "AC":
+        dispatch(acDelete());
+        break;
 
       default:
-        return "number";
+        console.log("default");
+        dispatch(addInput(input));
     }
   };
+
   return (
     <button
       className={`${classPicker(props.buttonValue)}`}
-      onClick={() => {
-        console.log(props.buttonValue);
-      }}
+      onClick={() => clickHandler(props.buttonValue)}
     >
       {props.buttonValue}
     </button>
--- src/features/calculator/calculatorSlice.js
+++ src/features/calculator/calculatorSlice.js
@@ -1,18 +1,44 @@
 import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
-  input: [0]
+  input: "",
+  answer: "",
 };
 const calculatorSlice = createSlice({
   name: "calculator",
   initialState,
   reducers: {
-   
+    addInput: (state, action) => {
+      state.input += action.payload;
+    },
+
+    calc: (state) => {
+      try {
+        state.answer = eval(state.input);
+      } catch (e) {
+        if (e instanceof SyntaxError) {
+          alert(e.message);
+        }
+      }
+      state.input = "";
+    },
+
+    singleOperator: (state, action) => {
+      if (state.input.slice(-1) == action.payload) {
+        return;
+      } else {
+        state.input += action.payload;
+      }
+    },
+    acDelete: (state) => {
+      state.input = state.input.slice(0, -1);
+    },
   },
 });
 
-
 export const selectInput = (state) => state.calculatorReducer.input;
+export const selectAnswer = (state) => state.calculatorReducer.answer;
 
-// export const { } = calculatorSlice.actions;
+export const { addInput, calc, singleOperator, acDelete } =
+  calculatorSlice.actions;
 
 export default calculatorSlice.reducer;
